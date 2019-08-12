5510a3cf77bb00636a6eb7b16a964e7ba13190eb
wanggang <758578893@qq.com>
2019-08-12 16:03:26 +0800

    Initial commit

---
+++ src/com/pers/algo/graphic/Demo/BorderLayoutDemo.java
@@ -0,0 +1,27 @@
+package com.pers.algo.graphic.Demo;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BorderLayoutDemo {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("this is a borderLayout test");
+        jFrame.setLayout(new BorderLayout());
+
+        JButton button1 = new JButton("上");
+        JButton button2 = new JButton("下");
+        JButton button3 = new JButton("左");
+        JButton button4 = new JButton("右");
+        JButton button5 = new JButton("中");
+
+        jFrame.add(button1,BorderLayout.NORTH);
+        jFrame.add(button2,BorderLayout.SOUTH);
+        jFrame.add(button3,BorderLayout.WEST);
+        jFrame.add(button4,BorderLayout.EAST);
+        jFrame.add(button5,BorderLayout.CENTER);
+
+        jFrame.setBounds(200, 200, 300, 300);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
