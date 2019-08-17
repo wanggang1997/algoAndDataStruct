83222b9fe769e28fc4ddea5f0650ab205cf11ed1
75857 <758578893@qq.com>
2019-08-17 11:01:03 +0800

    IndexMaxHeap reverse实现

--- src/com/pers/algo/nographic/heap/HeapSort.java
+++ src/com/pers/algo/nographic/heap/HeapSort.java
@@ -20,6 +20,15 @@ public class HeapSort {
         }
     }
 
-
+    public static void heapSortIndex(int []arr) throws Exception {
+        int len = arr.length;
+        IndexMaxHeap indexMaxHeap = new IndexMaxHeap(len);
+        for (int i = 0; i < len; i++) {
+            indexMaxHeap.insert(i,arr[i]);
+        }
+        for (int i = len-1; i >=0 ; i--) {
+            arr[i] = indexMaxHeap.extractMax();
+        }
+    }
 
 }
--- src/com/pers/algo/nographic/heap/Test.java
+++ src/com/pers/algo/nographic/heap/Test.java
@@ -5,38 +5,31 @@ import com.pers.algo.nographic.util.PrintUtil;
 
 public class Test {
 
-    public static void main(String[] args) {
-
-        MaxHeap maxHeap = new MaxHeap(10);
-        System.out.println(maxHeap.capacity());
-        for (int i = 1; i <=maxHeap.capacity() ; i++) {
-            int n = (int)(Math.random()*90+10);
-            maxHeap.insert(n);
-        }
-        maxHeap.printMaxHeap();
-        System.out.println();
-        while (!maxHeap.isEmpty()){
-            System.out.print(maxHeap.extractMax()+" ");
-        }
-        System.out.println();
+    public static void main(String[] args) throws Exception {
 
         //int [] arr1 = GenerateArrayUtil.generateRandomArray(10,0,10);
-        int [] arr1={9,8,7,6,5,4,3,2,1,0};
+        int [] arr1={9,8,2,6,5,4,7,3,1,7,0};
         HeapSort.heapSort1(arr1);
         System.out.print("普通最大堆排序：");
         PrintUtil.printfArrs(arr1);
 
         //int [] arr2 = GenerateArrayUtil.generateRandomArray(10,0,10);
-        int [] arr2={9,8,7,6,5,4,3,2,1,0};
+        int [] arr2={9,8,2,6,5,4,7,3,1,7,0};
         HeapSort.heapSort2(arr2);
         System.out.print("heapify式堆排序：");
         PrintUtil.printfArrs(arr2);
 
         //int [] arr3 = GenerateArrayUtil.generateRandomArray(10,0,10);
-        int [] arr3={9,8,7,6,5,4,3,2,1,0};
+        int [] arr3={9,8,2,6,5,4,7,3,1,7,0};
         InSituHeapSort.insituHeapSort(arr3);
         System.out.print("原地排序：");
         PrintUtil.printfArrs(arr3);
+
+        int [] arr4={9,8,2,6,5,4,7,3,1,7,0};
+        HeapSort.heapSortIndex(arr4);
+        System.out.print("最大索引堆：");
+        PrintUtil.printfArrs(arr4);
+
     }
 
 }
